a4ace0eeeb4f3fbbd0428d52b1e7907e510e3a6d
Maxwell Sarauer <105516956+RushThePenguin@users.noreply.github.com>
2022-09-09 14:17:17 +0000

    up

---
+++ prog52a.py
@@ -0,0 +1,11 @@
+length = 143
+width = 82
+
+print("The Length is", length)
+print("The width is", width)
+
+area = length * width
+perim = 2 * length + 2 * width 
+
+print("\nThe area is", area)
+print("The perimeter is", perim)
