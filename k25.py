66a78de0285e61deac2fa20cb6171c0e0bc64260
Maxwell Sarauer <105516956+RushThePenguin@users.noreply.github.com>
2022-09-12 14:10:43 +0000

    up

---
+++ k25.py
@@ -0,0 +1,25 @@
+from stanfordkarel import *
+
+
+def main():
+    """ Karel code goes here! """
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    pick_beeper()
+  
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
