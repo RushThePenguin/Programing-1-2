b783f145eb8b922060a13d819194d5a69b678b32
Maxwell Sarauer <105516956+RushThePenguin@users.noreply.github.com>
2022-09-15 14:33:46 +0000

    up

--- k35.py
+++ k35.py
@@ -10,22 +10,22 @@ class ktools:
     """Turn Left"""
     turn_left()
 
-   def tr(self):
+  def tr(self):
     """Turn Right"""
     self.tl()
     self.tl()
     self.tl()
 
-   def ta(self):
+  def ta(self):
     """Turn Around"""
     self.tl()
     self.tl()
 
-   def pick(self):
+  def pick(self):
     """Pick Beeper"""
     pick_beeper()
 
-   def put(self):
+  def put(self):
     """Put Beeper"""
     put_beeper()
 
@@ -45,28 +45,28 @@ class ktools:
 
   def h(self):
    """Print H using beepers"""
-    self.tl()
-    self.put5()
-    self.tr()
-    self.m()
-    self.m()
-    self.m()
-    self.tr()
-    self.put5()
-    self.ta()
-    self.m()
-    self.m()
-    self.tl()
-    self.m()
-    self.put2()
-    self.tl()
-    self.m()
-    self.m()
-    self.tl()
-    self.m()
-    self.m()
-    self.m()
-    self.m()
+   self.tl()
+   self.put5()
+   self.tr()
+   self.m()
+   self.m()
+   self.m()
+   self.tr()
+   self.put5()
+   self.ta()
+   self.m()
+   self.m()
+   self.tl()
+   self.m()
+   self.put2()
+   self.tl()
+   self.m()
+   self.m()
+   self.tl()
+   self.m()
+   self.m()
+   self.m()
+   self.m()
     
 def main():
     """ Karel code goes here! """
