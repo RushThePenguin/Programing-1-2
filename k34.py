a7e6c8e35ac31b709a89ab94db61ee6a2b2cc58c
Maxwell Sarauer <105516956+RushThePenguin@users.noreply.github.com>
2022-09-16 01:31:18 +0000

    up

--- k34.py
+++ k34.py
@@ -1,77 +1,97 @@
 from stanfordkarel import *
 
 
-class ktools:
-  def m(self):
-    """shorthand for move"""
-    move()
-    
-  def tl(self):
-    """Turn Left"""
-    turn_left()
-
-   def tr(self):
-    """Turn Right"""
-    self.tl()
-    self.tl()
-    self.tl()
-
-   def ta(self):
-    """Turn Around"""
-    self.tl()
-    self.tl()
-
-   def pick(self):
-    """Pick Beeper"""
-    pick_beeper()
-
-   def put(self):
-    """Put Beeper"""
-    put_beeper()
-
-  def put2(self):
-    """Put 2 beepers in a line"""
-    self.put()
-    self.m()
-    self.put()
-
-  def put5(self):
-    """Put 5 beepers in a line"""
-    self.put2()
-    self.m()
-    self.put2()
-    self.m()
-    self.put()
-
-  def h(self):
-   """Print H using beepers"""
-    self.tl()
-    self.put5()
-    self.tr()
-    self.m()
-    self.m()
-    self.m()
-    self.tr()
-    self.put5()
-    self.ta()
-    self.m()
-    self.m()
-    self.tl()
-    self.m()
-    self.put2()
-    self.tl()
-    self.m()
-    self.m()
-    self.tl()
-    self.m()
-    self.m()
-    self.m()
-    self.m()
-    
 def main():
     """ Karel code goes here! """
-    kt = ktools()
-    
+    beepers_in_bag()
+    move()
+    move()
+    move()
+    move()
+    move()
+    turn_left()
+    move()
+    put_beeper()
+    move()
+    move()
+    put_beeper()
+    move()
+    move()
+    put_beeper()
+    move()
+    move()
+    put_beeper()
+    turn_left()
+    move()
+    turn_left()
+    move()
+    put_beeper()
+    move()
+    move()
+    put_beeper()
+    move()
+    move()
+    put_beeper()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    move()
+    put_beeper()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    put_beeper()
+    turn_left()
+    move()
+    turn_left()
+    move()
+    put_beeper()
+    turn_left()
+    move()
+    move()
+    move()
+    move()
+    move()
+    move()
+    put_beeper()
+    turn_left()
+    move()
+    turn_left()
+    move()
+    put_beeper()
+    turn_left()
+    move()
+    move()
+    put_beeper()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    put_beeper()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    put_beeper()
+    move()
+    move()
+    put_beeper()
+    turn_left()
+    move()
+    turn_left()
+    move()
+    move()
+    move()
+    move()
+    move()
+    move()
+    turn_left()
+    turn_left()
     pass
 
 
