d4351098e214c5b065c71e59aafa6b5236161fc0
Maxwell Sarauer <105516956+RushThePenguin@users.noreply.github.com>
2022-11-14 15:38:59 +0000

    up

---
+++ LP4-3.py
@@ -0,0 +1,23 @@
+def main():
+  eggs = int(input("Enter the number of eggs purchased: "))
+  dozens = eggs/12
+  remainder = eggs % 12
+  cost = 0.0
+  price = 0.0
+  
+  if eggs > 0 and eggs < 4:
+    price = .5
+  if eggs >= 4 and eggs < 6:
+    price = .45
+  if eggs >= 6 and eggs < 11:
+    
+  if eggs >= 11:
+
+
+
+  cost = cost + (remainder)
+  pass
+
+
+if __name__== "__main__":
+  main()
