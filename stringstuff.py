27f1dd254edec6ba6fccd2a2e6b7fdb5d7b9a674
Maxwell Sarauer <105516956+RushThePenguin@users.noreply.github.com>
2022-11-15 15:03:46 +0000

    up

---
+++ stringstuff.py
@@ -0,0 +1,9 @@
+thing = "HELLO world HERE is SOME text"
+print(thing.lower())
+print(thing.upper())
+print(thing.isdigit())
+locworld = thing.find("world")
+print(locworld) # index0f
+print(thing[::-1])
+print(thing.split(' '))
+print("Hello" * 500000)
